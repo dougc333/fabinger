ac9f8abf75c28ab172fce04e769c13f7660bfcff
dougc333 <dougc@stanfordalumni.org>
2021-02-23 19:58:29 -0800

    add

--- grading/test/src/components/AllPages.jsx
+++ grading/test/src/components/AllPages.jsx
@@ -11,24 +11,39 @@ class AllPages extends React.Component{
 
     }
   }
-  
-  make() {
-    let arr=[];
-    const w = ["week1","week2","week3","week4"]
-    for (let i=0;i<w.length;i++){
-      arr.push(
-        <div> 
-            {w[i]}
-        </div>
-      )
+  //each week is a new href. 
+  //template page? and you create a new file for each week? 
+  parseData(){
+    let arr=[]
+    for (let x of Object.keys(data)){
+      //console.log(x)
+      console.log(data[x])
+      arr.push(<div>
+        <h4>{x}</h4>
+        </div>)
+    console.log("data[x]:",data[x])
+    console.log("data[x].length",data[x].length)
+    for (let y of Object.keys(data[x])){
+      console.log("y:",y)
+      console.log("data[x][y]:",data[x][y])
+      console.log("Object.keys(data[x][y])",Object.keys(data[x][y]))
+      for (let z of Object.keys(data[x][y])){
+        console.log("z:",z)
+        arr.push(<div>
+          {data[x][y][z]}
+        </div>)
+        console.log(data[x][y][z])
+      }
     }
-    return arr;    
+
+    }
+    return arr
   }
 
   render(){
     return(
       <div>
-      {this.make()}
+      {this.parseData()}
       </div>
     );
   }
